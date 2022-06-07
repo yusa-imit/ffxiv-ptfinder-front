70da0e6c694b6003f3d2ded64def44d8a64cacc4
yusa-imit <esimileiar81@gmail.com>
2022-06-07 20:01:16 +0900

    #2 add html div element props

--- src/components/Collapse.tsx
+++ src/components/Collapse.tsx
@@ -1,16 +1,22 @@
 import { ReactChild, ReactChildren, useState } from "react";
-interface CollapseProps {
+interface CollapseProps extends React.HTMLAttributes<HTMLDivElement> {
   title: string;
   content: ReactChild | ReactChildren;
 }
-export default function Collapse({ title, content }: CollapseProps) {
+export default function Collapse({
+  title,
+  content,
+  className,
+  ...etc
+}: CollapseProps) {
   const [open, setOpen] = useState(false);
   return (
     <div
       tabIndex={0}
       className={`collapse collapse-arrow border ${
         open === true ? "collapse-open" : "collapse-close"
-      } border-base-300 bg-base-200 rounded-box`}
+      } border-base-300 bg-base-200 rounded-box ${className}`}
+      {...etc}
       onClick={() => {
         setOpen((prev) => !prev);
       }}
--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -2,7 +2,7 @@ import { ResponsiveBreakpoints } from "../type/theme/responsiveBreakpoints";
 import { ColorTypes } from "../type/theme/colorTypes";
 import { getColorTypeClass, getSizeClass } from "src/lib/classNameFunction";
 import { ReactChild } from "react";
-interface TagProps {
+interface TagProps extends React.HTMLAttributes<HTMLDivElement> {
   data: string | ReactChild;
   size?: ResponsiveBreakpoints;
   colorType?: ColorTypes;
@@ -14,13 +14,16 @@ export default function Tag({
   size = "md",
   colorType,
   outline,
+  className,
+  ...etc
 }: TagProps): JSX.Element {
   return (
     <div
       className={`badge ${outline && "badge-outline"} ${getSizeClass(
         "badge",
         size
-      )} ${colorType && getColorTypeClass("badge", colorType)}`}
+      )} ${colorType && getColorTypeClass("badge", colorType)} ${className}`}
+      {...etc}
     >
       {data}
     </div>
