6da59633af1d71482cc5d9d4fd7202056621c516
yusa-imit <esimileiar81@gmail.com>
2022-06-10 20:33:48 +0900

    detect user prefer on dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,22 @@
 import { AppProps } from "next/app";
-import { RecoilRoot } from "recoil";
+import { RecoilRoot, useSetRecoilState } from "recoil";
 import { appWithTranslation } from "next-i18next";
 import "../styles/globals.css";
+import { useEffect } from "react";
+import { getThemeChangeHistory } from "src/lib/ThemeChangeHistory";
+import { theme } from "@recoil/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const setGlobal = useSetRecoilState(theme);
+  useEffect(() => {
+    if (!window) return;
+    const history = getThemeChangeHistory();
+    const isDark = window.matchMedia("(prefers-color-scheme: dark)");
+    if (isDark.matches) {
+      if (history.includes("light") || history.length > 0) return;
+      else setGlobal("dark");
+    }
+  }, [setGlobal]);
   return (
     <RecoilRoot>
       <Component {...pageProps} />
